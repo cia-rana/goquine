9a3fc7711680b5eb7d7e65b1ea7228f3b62b64c1
cia-rana <kiwamura0314@gmail.com>
2022-11-22 00:24:02 +0900

    add cia-rana3

---
+++ cia-rana3/main.go
@@ -0,0 +1,19 @@
+package main
+
+import . "fmt"
+
+func main() {
+	a += "\x60"
+	Println(a + a)
+}
+
+var a = `package main
+
+import . "fmt"
+
+func main() {
+	a += "\x60"
+	Println(a + a)
+}
+
+var a = `
